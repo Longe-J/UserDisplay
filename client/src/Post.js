ef03bdf9ccdd2eb830444fcb506bb5b4999b57a0
Longe-J <161849012+Longe-J@users.noreply.github.com>
2024-03-08 23:59:18 +0000

    Just a tiny bit of info added

---
+++ client/src/Post.js
@@ -0,0 +1,11 @@
+const Post = () => {
+    return ( 
+        <div className="post">
+            <h2>Name</h2>
+            <p>info</p>
+            <p>more infor</p>
+        </div>
+     );
+}
+ 
+export default Post;
